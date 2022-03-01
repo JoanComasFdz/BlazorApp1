df3c711e8d864e96f652969c24be71d02e00ec14
Joan Comas <joan.comas.fdz@gmail.com>
2022-03-01 15:21:35 +0100

    Add project files.

---
+++ ClassLibrary1/MySourceGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace ClassLibrary1
+{
+    [Generator]
+    public class MySourceGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            Console.WriteLine("Initializing generator...");
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            Console.WriteLine("Executing generator...");
+        }
+    }
+}
